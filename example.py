b4a150cc2060f633824085f7d40d1a42419ef106
Srinath Sridhar <srinath@brown.edu>
2022-06-06 22:33:57 -0400

    Modifications for STIC mini

--- example.py
+++ example.py
@@ -1,25 +1,43 @@
 from __future__ import print_function
 import uvc
 import logging
+import cv2
 
-logging.basicConfig(level=logging.INFO)
 
-dev_list = uvc.device_list()
-print(dev_list)
-cap = uvc.Capture(dev_list[0]["uid"])
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
 
-# Uncomment the following lines to configure the Pupil 200Hz IR cameras:
-# controls_dict = dict([(c.display_name, c) for c in cap.controls])
-# controls_dict['Auto Exposure Mode'].value = 1
-# controls_dict['Gamma'].value = 200
+    dev_list = uvc.device_list()
+    print(dev_list)
+    cap = uvc.Capture(dev_list[0]["uid"])
+    if cap is None:
+        print('No compatible UVC devices found. Exiting.')
+        exit()
 
-print(cap.avaible_modes)
-for x in range(10):
-    print(x)
-    cap.frame_mode = (640, 480, 30)
-    for x in range(100):
+    print('Available capture modes:', cap.available_modes)
+    print('Device name:', cap.name)
+    print('Bandwidth factor:', cap.bandwidth_factor)
+    # Uncomment the following lines to configure the Pupil 200Hz IR cameras:
+    controls_dict = dict([(c.display_name, c) for c in cap.controls])
+    print('Camera Controls:', controls_dict)
+    # controls_dict['Auto Exposure Mode'].value = 1
+    # controls_dict['Gamma'].value = 200
+
+    assert len(cap.available_modes) > 0
+
+    cap.frame_mode = cap.available_modes[-1]
+    print('Using capture mode:', cap.frame_mode)
+
+    while True:
         frame = cap.get_frame_robust()
-        print(frame.img.shape)
-        # cv2.imshow("img",frame.gray)
-        # cv2.waitKey(1)
-cap = None
+        # frame = cap.get_frame()
+        # print(frame.img.shape)
+
+        cv2.imshow('Live Capture',frame.img)
+        Key = cv2.waitKey(1)
+        if Key == 27:
+            break
+        if Key == ord('s'):
+            cv2.imwrite('frame.png', frame.img)
+
+    cap = None
--- multicam_example.py
+++ multicam_example.py
@@ -1,31 +1,124 @@
 from __future__ import print_function
+
+import numpy as np
 import uvc
 import logging
 import cv2
+import threading
+import time
+import math
+from datetime import datetime
 
-logging.basicConfig(level=logging.INFO)
+def getCurrentEpochTime():
+    return int((datetime.utcnow() - datetime(1970, 1, 1)).total_seconds() * 1e6)
 
-dev_list = uvc.device_list()
-dev_list = [dev_list[0]]
-print('Found {} cameras.'.format(len(dev_list)))
-Cams = []
-for i in range(len(dev_list)):
-    Cams.append(uvc.Capture(dev_list[i]["uid"]))
-    print('Supported modes:', Cams[i].avaible_modes)
-    Cams[i].frame_mode = (640, 480, 60)
+def makeCollage(ImageList, MaxWidth=800, FPSList=[]):
+    if ImageList is None:
+        return None
 
-#exit()
+    nImages = len(ImageList)
+    if nImages == 0:
+        return None
+    if nImages == 1:
+        return ImageList[0]
 
-# for x in range(10):
-for i in range(len(dev_list)):
-    frame = Cams[i].get_frame_robust()
-    print(frame.img.shape)
+    # Assuming images are all same size or we will resize to same size as the first image
+    Shape = ImageList[0].shape
+    for Ctr, Image in enumerate(ImageList, 0):
+        if len(FPSList) == len(ImageList):
+            cv2.putText(img=Image, text=str(math.floor(FPSList[Ctr])), org=(Image.shape[1]-math.floor(Image.shape[1]/10), math.floor(Image.shape[1]/10)),
+                        fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1.0, color=(0, 0, 255), thickness=2)
+        if Shape[0] != Image.shape[0] or Shape[1] != Image.shape[1]:
+            Image = cv2.resize(Image, Shape)
 
-# Uncomment the following lines to configure the Pupil 200Hz IR cameras:
-# controls_dict = dict([(c.display_name, c) for c in cap.controls])
-# controls_dict['Auto Exposure Mode'].value = 1
-# controls_dict['Gamma'].value = 200
+    nCols = math.ceil(math.sqrt(nImages))
+    nRows = math.ceil(nImages / nCols)
+    # print('nImages, Cols, Rows:', nImages, ',' ,nCols, ',', nRows)
 
-for i in range(len(dev_list)):
-    Cams[i] = None
+    Collage = np.zeros((Shape[0]*nRows, Shape[1]*nCols, Shape[2]), np.uint8)
+    for i in range(0, nImages):
+        Row = math.floor(i / nCols)
+        Col = i % nCols
+        Collage[Row*Shape[0]:Row*Shape[0]+Shape[0], Col*Shape[1]:Col*Shape[1]+Shape[1], :] = ImageList[i].copy()
+
+    if Collage.shape[1] > MaxWidth:
+        Fact = Collage.shape[1] / MaxWidth
+        NewHeight = round(Collage.shape[0] / Fact)
+        Collage = cv2.resize(Collage, (MaxWidth, NewHeight))
+
+    return Collage
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+
+    dev_list = uvc.device_list()
+    # dev_list = [dev_list[0]]
+    nCams = len(dev_list)
+    print('Found {} cameras.'.format(nCams))
+    Cams = []
+    for i in range(len(dev_list)):
+        Cams.append(uvc.Capture(dev_list[i]["uid"]))
+        print('Supported modes:', Cams[i].avaible_modes)
+        Cams[i].frame_mode = Cams[i].avaible_modes[-1]
+
+    CapturedFrames = [None]*nCams
+    DummyImage = np.empty((Cams[i].frame_mode[1], Cams[i].frame_mode[0], 3))
+    Stop = False
+    FPS = [0]*nCams
+    lock = threading.Lock()
+    def grab_frame(num):
+        global Stop
+        global FPS
+        global lock
+        while True:
+            startTime = getCurrentEpochTime()
+            CapturedFrames[num] = Cams[num].get_frame_robust()
+            # print("Cam: {} shape: {}".format(num, CapturedFrames[num].img.shape))
+            endTime = getCurrentEpochTime()
+            ElapsedTime = (endTime - startTime)
+            if ElapsedTime < 1000:
+                time.sleep(0.001)  # Prevent CPU throttling
+                ElapsedTime += 1000
+            with lock:
+                FPS[num] = 1e6 / (ElapsedTime)
+            # print('FPS:', num, math.floor(FPS[num]), flush=True)
+
+            if Stop:
+                break
+
+    def display():
+        global Stop
+        global FPS
+        global lock
+        while len(CapturedFrames) > 0:
+            if CapturedFrames[0] is not None:
+                with lock:
+                    ImageList = []
+                    for frame in CapturedFrames:
+                        if frame is not None:
+                            ImageList.append(frame.img)
+                    Collage = makeCollage(ImageList, MaxWidth=1200, FPSList=FPS)
+                cv2.imshow('Live Capture', Collage)
+            else:
+                cv2.imshow('Live Capture', DummyImage)
+            Key = cv2.waitKey(1)
+            if Key == 27:
+                with lock:
+                    Stop = True
+                break
+
+    Threads = []
+    DispThread = threading.Thread(target=display)
+    for i in range(nCams):
+        Threads.append(threading.Thread(target=grab_frame, args=(i,)))
+    for i in range(nCams):
+        Threads[i].start()
+    DispThread.start()
+    DispThread.join()
+    for i in range(nCams):
+        Threads[i].join()
+
+    for i in range(len(dev_list)):
+        Cams[i] = None
 
