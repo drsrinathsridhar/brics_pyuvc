c4d7ff7c0ff1bc298b713bc564f6b0615ae0b2cc
Srinath Sridhar <srinath@brown.edu>
2022-06-16 12:06:35 -0400

    updates

--- multicam_example.py
+++ multicam_example.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 
+import random
 import numpy as np
 import uvc
 import logging
@@ -8,6 +9,10 @@ import threading
 import time
 import math
 from datetime import datetime
+import argparse, sys
+
+Parser = argparse.ArgumentParser(description='Sample script to stream from UVC cameras.')
+Parser.add_argument('-i', '--id', help='Which camera IDs to use.', nargs='+', type=int, required=False, default=[])
 
 def getCurrentEpochTime():
     return int((datetime.utcnow() - datetime(1970, 1, 1)).total_seconds() * 1e6)
@@ -50,23 +55,35 @@ def makeCollage(ImageList, MaxWidth=800, FPSList=[]):
 
 
 if __name__ == '__main__':
+    # if len(sys.argv) == 1:
+    #     Parser.print_help()
+    #     exit()
+    Args = Parser.parse_args()
+
     logging.basicConfig(level=logging.INFO)
 
     dev_list = uvc.device_list()
-    # dev_list = [dev_list[0]]
+    # random.shuffle(dev_list)
     nCams = len(dev_list)
-    print('Found {} cameras.'.format(nCams))
-    nCams = 6
-    print('WARNING: Limiting to {} cameras.'.format(nCams))
+    assert nCams > 0
+    CamIdx = list(range(nCams))
+    print('Found {} cameras with indices: {}.'.format(nCams, CamIdx))
+    if Args.id != []:
+        CamIdx = Args.id
+    print('Limiting to camera indices: {}'.format(CamIdx))
+    nCams = len(CamIdx)
     Cams = []
-    for i in range(nCams):
+    for i in CamIdx:
         Cams.append(uvc.Capture(dev_list[i]["uid"]))
-        print('Supported modes:', Cams[i].avaible_modes)
-        Cams[i].frame_mode = Cams[i].avaible_modes[1]
-        Cams[i].bandwidth_factor = 0.9
+        controls_dict = dict([(c.display_name, c) for c in Cams[-1].controls])
+        print('Camera in Bus:ID -', dev_list[i]['uid'], 'supports the following modes:', Cams[-1].avaible_modes)
+        Cams[-1].frame_mode = Cams[-1].avaible_modes[-1]
+        Cams[-1].bandwidth_factor = 2
+        # time.sleep(1.0)
 
-    CapturedFrames = [None]*nCams
-    DummyImage = np.empty((Cams[i].frame_mode[1], Cams[i].frame_mode[0], 3))
+    print(len(Cams))
+    DummyFrame = np.zeros((Cams[0].frame_mode[1], Cams[0].frame_mode[0], 3))
+    CapturedFrames = [DummyFrame]*nCams
     Stop = False
     FPS = [0]*nCams
     lock = threading.Lock()
@@ -74,17 +91,23 @@ if __name__ == '__main__':
         global Stop
         global FPS
         global lock
+        global CapturedFrames
         while True:
             startTime = getCurrentEpochTime()
-            CapturedFrames[num] = Cams[num].get_frame_robust()
+            Frame = Cams[num].get_frame_robust()
+            lock.acquire()
+            CapturedFrames[num] = np.copy(Frame.img)
+            lock.release()
+            # CapturedFrames[num] = Cams[num].get_frame()
             # print("Cam: {} shape: {}".format(num, CapturedFrames[num].img.shape))
             endTime = getCurrentEpochTime()
             ElapsedTime = (endTime - startTime)
             if ElapsedTime < 1000:
                 time.sleep(0.001)  # Prevent CPU throttling
                 ElapsedTime += 1000
-            with lock:
-                FPS[num] = 1e6 / (ElapsedTime)
+            lock.acquire()
+            FPS[num] = 1e6 / (ElapsedTime)
+            lock.release()
             # print('FPS:', num, math.floor(FPS[num]), flush=True)
 
             if Stop:
@@ -94,34 +117,29 @@ if __name__ == '__main__':
         global Stop
         global FPS
         global lock
-        while len(CapturedFrames) > 0:
-            if CapturedFrames[0] is not None:
-                with lock:
-                    ImageList = []
-                    for frame in CapturedFrames:
-                        if frame is not None:
-                            ImageList.append(frame.img)
-                    Collage = makeCollage(ImageList, MaxWidth=1600, FPSList=FPS)
-                cv2.imshow('Live Capture', Collage)
-            else:
-                cv2.imshow('Live Capture', DummyImage)
+        global CapturedFrames
+        while True:
+            lock.acquire()
+            Collage = makeCollage(CapturedFrames, MaxWidth=800, FPSList=FPS)
+            # Collage = DummyFrame
+            lock.release()
+            cv2.imshow('Live Capture', Collage)
             Key = cv2.waitKey(1)
             if Key == 27:
-                with lock:
-                    Stop = True
+                lock.acquire()
+                Stop = True
+                lock.release()
                 break
 
     Threads = []
     DispThread = threading.Thread(target=display)
     for i in range(nCams):
         Threads.append(threading.Thread(target=grab_frame, args=(i,)))
+
+    DispThread.start()
     for i in range(nCams):
         Threads[i].start()
-    DispThread.start()
+
     DispThread.join()
     for i in range(nCams):
         Threads[i].join()
-
-    for i in range(len(dev_list)):
-        Cams[i] = None
-
