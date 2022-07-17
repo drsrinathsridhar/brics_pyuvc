52a46fc11ac7ef8fa1497f070b23788917d42590
Srinath Sridhar <srinath@raman.myfiosgateway.com>
2022-07-16 23:22:37 -0400

    def port

--- sticradio/example_client.py
+++ sticradio/example_client.py
@@ -7,9 +7,6 @@ Parser.add_argument('-o', '--hostname', help='Hostname or IP address.', type=str
 Parser.add_argument('-p', '--port', help='Port number on host.', type=str, default='8875')
 
 if __name__ == '__main__':
-    # if len(sys.argv) >= 1:
-    #     Parser.print_help()
-    #     exit()
     Args = Parser.parse_args()
 
     Client = sticradio.STICRadioClient(hostname=Args.hostname, port=Args.port)
--- sticradio/example_server.py
+++ sticradio/example_server.py
@@ -1,5 +1,12 @@
 import sticradio
+import argparse
+import sys
+
+Parser = argparse.ArgumentParser(description='Example script to create a websocket ping/pong server.')
+Parser.add_argument('-p', '--port', help='Port number on host.', type=str, default='8181')
 
 if __name__ == '__main__':
-    Server = sticradio.STICRadioServer(port='8080')
+    Args = Parser.parse_args()
+
+    Server = sticradio.STICRadioServer(port=Args.port)
     Server.start()
